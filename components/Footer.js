779835d1adc3078a906dd39143ad4648e60fbcf5
mohitb07 <bmohit980@gmail.com>
2021-10-24 21:43:38 +0530

    footer links updated

--- components/Footer.js
+++ components/Footer.js
@@ -7,10 +7,8 @@ function Footer() {
     return (
         <div className="flex flex-col justify-center h-52 items-center bg-[#222222] space-y-4">
             <div className="flex space-x-2">
-                <a className="rounded-none p-2 text-4xl hover:scale-110 hover:bg-blue-900 transition-all ease-out bg-[#333333]"><FaLinkedin/></a>
-                <a className="hover:bg-blue-900 rounded-none hover:scale-110 transition-all ease-out  p-2 text-4xl bg-[#333333]"><AiFillGithub/></a>
-                <a className="hover:bg-blue-900 rounded-none hover:scale-110 transition-all ease-out p-2 text-4xl bg-[#333333]"><AiFillGithub/></a>
-                <a className="hover:bg-blue-900 rounded-none hover:scale-110 transition-all ease-out p-2 text-4xl bg-[#333333]"><AiFillGithub/></a>
+                <a href="https://www.linkedin.com/in/mohitbisht19/"  target="_blank" rel="noreferrer" className="rounded-none p-2 text-4xl hover:scale-110 hover:bg-blue-900 transition-all ease-out bg-[#333333]"><FaLinkedin/></a>
+                <a href="https://github.com/Mohitb07" target="_blank" rel="noreferrer" className="hover:bg-blue-900 rounded-none hover:scale-110 transition-all ease-out  p-2 text-4xl bg-[#333333]"><AiFillGithub/></a>
             </div>
             <div className="text-4xl rotate-0 cursor-pointer">
                 <IoMdRocket onClick={() => window.scrollTo(0,0)}/>
