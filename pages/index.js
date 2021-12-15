45e42dde121f2666a7c39002987eb25b2f968e78
mohitb07 <bmohit980@gmail.com>
2021-12-15 23:04:06 +0530

    favicon updated

--- pages/index.js
+++ pages/index.js
@@ -1,29 +1,33 @@
-import React, { useEffect } from 'react'
-import Navbar from '../components/Navbar'
-import Hero from '../components/Hero'
-// import Test from '../components/test'
-import About from '../components/About';
-import Skills from '../components/Skills'
-import Projects from '../components/Projects';
-import Contact from '../components/Contact';
-import Resume from '../components/Resume';
-import Footer from '../components/Footer'
+import React from "react";
+import Head from "next/head";
+
+import Navbar from "../components/Navbar";
+import Hero from "../components/Hero";
+import About from "../components/About";
+import Skills from "../components/Skills";
+import Projects from "../components/Projects";
+import Contact from "../components/Contact";
+import Resume from "../components/Resume";
+import Footer from "../components/Footer";
 
 function Index() {
   return (
     <>
+      <Head>
+        <title>Mohit Bisht</title>
+      </Head>
       <div className="bg-white">
-        <Navbar/>
-        <Hero/>
-        <About/>
-        <Skills/>
-        <Resume/>
-        <Projects/>
-        <Contact/>
-        <Footer/>
+        <Navbar />
+        <Hero />
+        <About />
+        <Skills />
+        <Resume />
+        <Projects />
+        <Contact />
+        <Footer />
       </div>
     </>
-  )
+  );
 }
 
-export default Index
+export default Index;
