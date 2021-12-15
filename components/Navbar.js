b5a36181b2d67a3b2b6799fcc7d40c321da26c97
mohitb07 <bmohit980@gmail.com>
2021-12-15 23:02:20 +0530

    fixed navbar in smaller devices

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,58 +1,66 @@
 /* This example requires Tailwind CSS v2.0+ */
-import { Fragment, useEffect } from 'react'
-import { Disclosure, Menu, Transition } from '@headlessui/react'
-import { BellIcon, MenuIcon, XIcon } from '@heroicons/react/outline'
+import { Fragment, useEffect } from "react";
+import { Disclosure, Menu, Transition } from "@headlessui/react";
+import { BellIcon, MenuIcon, XIcon } from "@heroicons/react/outline";
 
 const navigation = [
-  { name: 'About', href: '#about', current: false },
-  { name: 'Skills', href: '#skills', current: false },
-  { name: 'Projects', href: '#projects', current: false },
-  { name: 'Contact', href: '#contact', current: false },
-]
+  { name: "About", href: "#about", current: false },
+  { name: "Skills", href: "#skills", current: false },
+  { name: "Projects", href: "#projects", current: false },
+  { name: "Contact", href: "#contact", current: false },
+];
 
 function classNames(...classes) {
-  return classes.filter(Boolean).join(' ')
+  return classes.filter(Boolean).join(" ");
 }
 
 export default function Navbar() {
   useEffect(() => {
-    window.addEventListener('scroll', function(){
-      let header = document.querySelector('nav');
-      header.classList.toggle('nav-scrolled', window.scrollY > 0)
-    })
-  })
- 
+    window.addEventListener("scroll", function () {
+      let header = document.querySelector("nav");
+      header.classList.toggle("nav-scrolled", window.scrollY > 0);
+    });
+  });
+
   return (
-    <Disclosure as="nav" className="z-50 w-screen lg:w-screen sticky top-0 transition-all ease-in-out duration-500">
+    <Disclosure
+      as="nav"
+      className="z-50 w-screen lg:w-screen sticky top-0 transition-all ease-in-out duration-500"
+    >
       {({ open }) => (
         <>
-          <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
+          <div
+            className={`max-w-7xl mx-auto px-2 sm:px-6 ${
+              open && "bg-black"
+            } lg:px-8 lg:bg-transparent`}
+          >
             <div className="relative flex items-center justify-between h-16">
               <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
                 {/* Mobile menu button*/}
-                <Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                <Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none">
                   <span className="sr-only">Open main menu</span>
                   {open ? (
-                    <XIcon className="block h-6 w-6" aria-hidden="true" />
+                    <XIcon
+                      className="block h-6 w-6 transition-all ease-in-out duration-300"
+                      aria-hidden="true"
+                    />
                   ) : (
-                    <MenuIcon className="block h-6 w-6" aria-hidden="true" />
+                    <MenuIcon
+                      className="block h-6 w-6 transition-all ease-in-out duration-300"
+                      aria-hidden="true"
+                    />
                   )}
                 </Disclosure.Button>
               </div>
               <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
                 <div className="flex-shrink-0 flex items-center">
-                  {/* <img
-                    className="block lg:hidden h-8 w-auto"
-                    src="https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg"
-                    alt="Workflow"
-                  /> */}
-                  <h1 className="block font-bold mt-3 lg:hidden h-8 w-auto">Mohit Bisht</h1>
-                  {/* <img
-                    className="hidden lg:block h-8 w-auto"
-                    src="https://tailwindui.com/img/logos/workflow-logo-indigo-500-mark-white-text.svg"
-                    alt="Workflow"
-                  /> */}
-                  <h1 className="hidden lg:block h-8 w-auto font-bold mt-1 text-lg">Mohit Bisht</h1>
+                  <h1 className="block uppercase tracking-wider font-bold mt-3 lg:hidden h-8 w-auto">
+                    Mohit Bisht
+                  </h1>
+
+                  <h1 className="hidden uppercase tracking-wider lg:block h-8 w-auto font-bold mt-1 text-lg">
+                    Mohit Bisht
+                  </h1>
                 </div>
                 <div className="hidden sm:block sm:ml-6">
                   <div className="flex space-x-4">
@@ -61,10 +69,12 @@ export default function Navbar() {
                         key={item.name}
                         href={item.href}
                         className={classNames(
-                          item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-                          'px-3 py-2 rounded-md text-sm font-medium'
+                          item.current
+                            ? "bg-gray-900 text-white"
+                            : "text-gray-300 hover:bg-gray-700 hover:text-white",
+                          "px-3 py-2 rounded-md text-sm font-medium"
                         )}
-                        aria-current={item.current ? 'page' : undefined}
+                        aria-current={item.current ? "page" : undefined}
                       >
                         {item.name}
                       </a>
@@ -72,26 +82,22 @@ export default function Navbar() {
                   </div>
                 </div>
               </div>
-              <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-              
-
-                {/* Profile dropdown */}
-                
-              </div>
             </div>
           </div>
 
           <Disclosure.Panel className="sm:hidden">
-            <div className="px-2 pt-2 pb-3 space-y-1">
+            <div className="px-2 pt-2 pb-3 space-y-1 bg-black transition-all ease-in-out duration-300">
               {navigation.map((item) => (
                 <a
                   key={item.name}
                   href={item.href}
                   className={classNames(
-                    item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-                    'block px-3 py-2 rounded-md text-base font-medium'
+                    item.current
+                      ? "bg-gray-900 text-white"
+                      : "text-gray-300 hover:bg-gray-700 hover:text-white",
+                    "block px-3 py-2 rounded-md text-base font-medium"
                   )}
-                  aria-current={item.current ? 'page' : undefined}
+                  aria-current={item.current ? "page" : undefined}
                 >
                   {item.name}
                 </a>
@@ -101,5 +107,5 @@ export default function Navbar() {
         </>
       )}
     </Disclosure>
-  )
+  );
 }
