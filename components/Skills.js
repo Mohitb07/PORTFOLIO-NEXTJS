f45717785fb2f0c84c842465319526012d37c455
mohitb07 <bmohit980@gmail.com>
2022-09-25 22:53:59 +0530

    new skills added and rendering logic changed

--- components/Skills.js
+++ components/Skills.js
@@ -2,7 +2,7 @@ import Image from "next/image";
 import React from "react";
 
 import css from "../public/css.png";
-import docker2 from "../public/docker2.png";
+import docker from "../public/docker.png";
 import firebase from "../public/firebase.png";
 import git from "../public/git.png";
 import html from "../public/html.png";
@@ -14,106 +14,49 @@ import react from "../public/react.png";
 import redux from "../public/redux.png";
 import sql from "../public/sql.png";
 import tailwind from "../public/tailwind.png";
+import typescript from "../public/ts.png";
+import github from "../public/github.png";
+import yarn from "../public/yarn.png";
+import sass from "../public/sass.png";
+
+const dataSource = [
+  { src: html, alt: "html" },
+  { src: css, alt: "css" },
+  { src: js, alt: "js" },
+  { src: typescript, alt: "typescript" },
+  { src: react, alt: "react" },
+  { src: nodejs, alt: "nodejs" },
+  { src: tailwind, alt: "tailwind" },
+  { src: redux, alt: "redux" },
+  { src: mongo, alt: "mongo" },
+  { src: sql, alt: "sql" },
+  { src: git, alt: "git" },
+  { src: postman, alt: "postman" },
+  { src: docker, alt: "docker" },
+  { src: firebase, alt: "firebase" },
+  { src: github, alt: "github" },
+  { src: yarn, alt: "yarn" },
+  { src: sass, alt: "sass" },
+];
 
 function Skills() {
   return (
     <div className="flex flex-col justify-center items-center space-y-7 h-screen">
       <h1 className="text-4xl text-[#CCD6F6] font-semibold">SKILLS</h1>
       <h3 className="text-xs lg:text-base text-center opacity-80 text-[#8892B0]">
-        SOME TECHONOLOGIES I&apos;`VE WORKED WITH:
+        SOME TECHONOLOGIES I&apos;VE WORKED WITH:
       </h3>
       <div className="flex flex-wrap gap-5 justify-center md:max-w-2xl item-center max-w-xs">
-        <Image
-          className="object-contain fitler grayscale hover:grayscale-0 transition-all ease-out"
-          src={html}
-          width={100}
-          height={80}
-          alt="skill"
-        />
-        <Image
-          className="object-contain fitler grayscale hover:grayscale-0 transition-all ease-out"
-          src={css}
-          width={100}
-          height={80}
-          alt="skill"
-        />
-        <Image
-          className="object-contain fitler grayscale hover:grayscale-0 transition-all ease-out"
-          src={js}
-          width={100}
-          height={80}
-          alt="skill"
-        />
-        <Image
-          className="object-contain fitler grayscale hover:grayscale-0 transition-all ease-out"
-          src={react}
-          width={100}
-          height={80}
-          alt="skill"
-        />
-        <Image
-          className="object-contain fitler grayscale hover:grayscale-0 transition-all ease-out"
-          src={nodejs}
-          width={100}
-          height={80}
-          alt="skill"
-        />
-        <Image
-          className="object-contain fitler grayscale hover:grayscale-0 transition-all ease-out"
-          src={tailwind}
-          width={100}
-          height={80}
-          alt="skill"
-        />
-        <Image
-          className="object-contain fitler grayscale hover:grayscale-0 transition-all ease-out"
-          src={redux}
-          width={100}
-          height={80}
-          alt="skill"
-        />
-        <Image
-          className="object-contain fitler grayscale hover:grayscale-0 transition-all ease-out"
-          src={mongo}
-          width={100}
-          height={80}
-          alt="skill"
-        />
-        <Image
-          className="object-contain fitler grayscale hover:grayscale-0 transition-all ease-out"
-          src={sql}
-          width={100}
-          height={80}
-          alt="skill"
-        />
-        <Image
-          className="object-contain fitler grayscale hover:grayscale-0 transition-all ease-out"
-          src={git}
-          width={100}
-          height={80}
-          alt="skill"
-        />
-        <Image
-          className="object-contain fitler grayscale hover:grayscale-0 transition-all ease-out"
-          src={postman}
-          width={100}
-          height={80}
-          alt="skill"
-        />
-        <Image
-          className="object-contain fitler grayscale hover:grayscale-0 transition-all ease-out"
-          src={docker2}
-          width={100}
-          height={80}
-          alt="skill"
-        />
-        <Image
-          className="object-contain fitler grayscale hover:grayscale-0 transition-all ease-out"
-          src={firebase}
-          width={100}
-          height={80}
-          alt="skill"
-        />
+        {dataSource.map(({ alt, src }) => (
+          <Image
+            key={alt}
+            className="object-contain fitler grayscale hover:grayscale-0 transition-all ease-out"
+            src={src}
+            width={100}
+            height={80}
+            alt={alt}
+          />
+        ))}
       </div>
     </div>
   );
