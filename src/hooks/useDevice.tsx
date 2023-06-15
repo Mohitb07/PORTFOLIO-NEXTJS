0e371453886ba870217b28af74c0a923735de0e6
Mohitb07 <bmohit980@gmail.com>
2023-06-15 20:40:26 +0530

    feat: to check device type custom hook added

---
+++ src/hooks/useDevice.tsx
@@ -0,0 +1,15 @@
+import { useEffect, useState } from "react";
+
+export const useDevice = () => {
+  const [isMobile, setIsMobile] = useState(false);
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth < 768);
+    };
+    handleResize();
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  return isMobile;
+};
