2d4b7164be8822709ee8cb9b7936f123b42c535a
Mohitb07 <bmohit980@gmail.com>
2023-06-15 20:41:08 +0530

    refactor: now change height and width based on device type

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,23 +1,26 @@
 import Image from "next/image";
 import React from "react";
 
-import data from '../data/data.json'
+import data from "../data/data.json";
+import { useDevice } from "../hooks/useDevice";
 
 function Skills() {
+  const isMobile = useDevice();
+  console.log("isMobile", isMobile);
+  const width = isMobile ? 70 : 100;
+  const height = isMobile ? 70 : 80;
   return (
     <div className="flex flex-col justify-center items-center space-y-7 pt-20 pb-40">
       <h1 className="base-title">SKILLS</h1>
-      <p className="sub-title">
-        SOME TECHONOLOGIES I&apos;VE WORKED WITH:
-      </p>
+      <p className="sub-title">SOME TECHONOLOGIES I&apos;VE WORKED WITH:</p>
       <div className="flex flex-wrap gap-5 justify-center md:max-w-2xl item-center">
         {data.skills.map(({ alt, src }) => (
           <Image
             key={alt}
             className="object-contain fitler grayscale hover:grayscale-0 transition-all ease-out"
             src={src}
-            width={100}
-            height={80}
+            width={width}
+            height={height}
             alt={alt}
           />
         ))}
