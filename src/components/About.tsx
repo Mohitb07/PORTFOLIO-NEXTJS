2a1e2fd1f5de086ccf5cdbe045d47ba37651d393
mohitb07 <bmohit980@gmail.com>
2024-09-24 22:04:19 +0530

    refactor: update component layout and content

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -4,12 +4,12 @@ import { SiAzuredevops } from "react-icons/si";
 import { VscServerProcess } from "react-icons/vsc";
 import Image from "next/image";
 
-import data from '../data/data.json'
+import data from "../data/data.json";
 
 function About() {
   return (
-    <div>
-      <div className="mt-10 flex flex-col text-[#CCD6F6] p-10 items-center w-full">
+    <div id="about">
+      <div className="flex flex-col text-[#CCD6F6] p-10 items-center max-w-7xl mx-auto">
         <h1 className="md:base-title text-3xl font-light">A LITTLE ABOUT ME</h1>
         <div className="avatar my-10">
           <div className="w-40 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
@@ -23,54 +23,64 @@ function About() {
         </div>
 
         <div className="lg:max-w-lg text-center mb-24 max-w-screen-sm text-[#8892B0] select-none">
-          <p className="leading-8">
-            {data.about.aboutMe}
-          </p>
+          <p className="leading-8">{data.about.aboutMe}</p>
         </div>
-        <div className="mt-52">
-          <h1 className="base-title text-center">
-            WHAT I DO
-          </h1>
-          <div className="mt-20 mb-52 lg:flex justify-center space-y-3 lg:space-x-8 items-center">
-            <div className="flex flex-col items-center space-y-8 lg:max-w-lg">
-              <FaReact className="text-[10rem] bg-[#793EF9] text-[#CCD6F6] rounded-full p-12 hover:animate-spin" />
+        <div className="my-[10%] flex flex-col">
+          <h1 className="base-title text-center">WHAT I DO</h1>
+          <div className="p-2 grid lg:grid-cols-3 my-10 gap-10 md:gap-5 lg:gap-10">
+            <div className="flex flex-col items-center space-y-4">
+              <FaReact className="text-[8rem] lg:text-[10rem] bg-[#793EF9] text-[#CCD6F6] rounded-full p-8 lg:p-12 hover:animate-spin" />
               <h3>Front End</h3>
-              <p className="text-center opacity-70">
-                Most of the time i work with{" "}
-                <span className="font-semibold">ReactJS, </span>
-                <span className="font-semibold">NextJS </span>
-                and doing it for the past three years, and I know HTML, CSS, JS,
-                Client-Side Rendering, and Tailwind in this area.
+              <p className="text-center opacity-70 text-sm lg:text-base">
+                I've primarily worked with{" "}
+                <span className="font-semibold text-[#793EF9]">ReactJS </span>
+                and
+                <span className="font-semibold text-[#793EF9]"> NextJS </span>
+                for the past four years, with experience in HTML, CSS, JS,
+                client-side rendering, and Tailwind.
               </p>
             </div>
 
-            <div className="flex flex-col items-center space-y-8 lg:max-w-lg">
-              <VscServerProcess className="text-[10rem] bg-[#793EF9] text-[#CCD6F6] rounded-full p-12 hover:animate-spin" />
+            <div className="flex flex-col items-center space-y-4 lg:max-w-lg">
+              <VscServerProcess className="text-[8rem] lg:text-[10rem] bg-[#793EF9] text-[#CCD6F6] rounded-full p-8 lg:p-12 hover:animate-spin" />
               <h3>Back End</h3>
-              <p className="text-center opacity-70">
-                I have also worked in a NodeJS environment and have done things
-                like <span className="font-semibold">RestAPIs</span>,
-                <span className="font-semibold">
-                  Authentication/Authorization
+              <p className="text-center opacity-70 text-sm lg:text-base">
+                I've worked with NodeJS on tasks like
+                <span className="font-semibold text-[#793EF9]"> RestAPIs</span>,
+                <span className="font-semibold text-[#793EF9]">
+                  {" "}
+                  authentication/authorization
                 </span>
                 , and
-                <span className="font-semibold"> Server Side Rendering</span>. I
-                am also just familiar with{" "}
-                <span className="font-semibold"> Microservices </span>{" "}
-                Architecture.{" "}
+                <span className="font-semibold text-[#793EF9]">
+                  {" "}
+                  server side rendering
+                </span>
+                . I'm also familiar with{" "}
+                <span className="font-semibold text-[#793EF9]">
+                  {" "}
+                  microservices{" "}
+                </span>{" "}
+                architecture.{" "}
               </p>
             </div>
 
-            <div className="flex flex-col items-center space-y-8 lg:max-w-lg">
-              <SiAzuredevops className="text-[10rem] bg-[#793EF9] text-[#CCD6F6] rounded-full p-12 hover:animate-spin" />
+            <div className="flex flex-col items-center space-y-4 lg:max-w-lg">
+              <SiAzuredevops className="text-[8rem] lg:text-[10rem] bg-[#793EF9] text-[#CCD6F6] rounded-full p-8 lg:p-12 hover:animate-spin" />
               <h3>DevOps</h3>
-              <p id="skills" className="text-center opacity-70">
-                Sometimes I also do some DevOps things like deploying to{" "}
-                <span className="font-semibold">Kubernetes</span> cluster,
-                isolating the environment with{" "}
-                <span className="font-semibold">Docker</span> and using{" "}
-                <span className="font-semibold">Git</span> for version control,
-                but I am just familiar with Docker and Kubernetes for now.{" "}
+              <p
+                id="skills"
+                className="text-center opacity-70 text-sm lg:text-base"
+              >
+                I occasionally work on DevOps tasks like deploying to{" "}
+                <span className="font-semibold text-[#793EF9]">
+                  Kubernetes{" "}
+                </span>
+                isolating environments with{" "}
+                <span className="font-semibold text-[#793EF9]">Docker</span> and
+                using <span className="font-semibold text-[#793EF9]">Git</span>{" "}
+                for version control. However, I'm only familiar with Docker and
+                Kubernetes for now.{" "}
               </p>
             </div>
           </div>
