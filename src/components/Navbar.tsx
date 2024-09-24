88c97b0245045816886a8549380be4037510e9a8
mohitb07 <bmohit980@gmail.com>
2024-09-24 22:07:09 +0530

    refactor: Add close functionality to mobile menu links and code refactored

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,12 @@
-import { useEffect } from "react";
-import { Disclosure } from "@headlessui/react";
-import { MenuIcon, XIcon } from "@heroicons/react/outline";
+import { useLayoutEffect } from "react";
+import {
+  Disclosure,
+  DisclosureButton,
+  DisclosurePanel,
+} from "@headlessui/react";
+import {XMarkIcon, Bars3Icon } from "@heroicons/react/24/solid";
 
-import data from '../data/data.json'
+import data from "../data/data.json";
 
 const navigation = [
   { name: "About", href: "#about", current: false },
@@ -16,7 +20,7 @@ function classNames(...classes: string[]) {
 }
 
 export default function Navbar() {
-  useEffect(() => {
+  useLayoutEffect(() => {
     window.addEventListener("scroll", function () {
       let header = document.querySelector("nav");
       header.classList.toggle("nav-scrolled", window.scrollY > 0);
@@ -40,20 +44,20 @@ export default function Navbar() {
             <div className="relative flex items-center justify-between h-16">
               <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
                 {/* Mobile menu button*/}
-                <Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none">
+                <DisclosureButton className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none">
                   <span className="sr-only">Open main menu</span>
                   {open ? (
-                    <XIcon
+                    <XMarkIcon
                       className="block h-6 w-6 transition-all ease-in-out duration-300"
                       aria-hidden="true"
                     />
                   ) : (
-                    <MenuIcon
+                    <Bars3Icon
                       className="block h-6 w-6 transition-all ease-in-out duration-300"
                       aria-hidden="true"
                     />
                   )}
-                </Disclosure.Button>
+                </DisclosureButton>
               </div>
               <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
                 <div className="flex-shrink-0 flex items-center">
@@ -65,7 +69,7 @@ export default function Navbar() {
                     {data.brand}
                   </h1>
                 </div>
-                <div className="hidden sm:block sm:ml-6">
+                <div className="hidden md:block sm:ml-6">
                   <div className="flex space-x-4">
                     {navigation.map((item) => (
                       <a
@@ -73,7 +77,7 @@ export default function Navbar() {
                         href={item.href}
                         className={classNames(
                           item.current
-                            ? "bg-gray-900 text-white"
+                            ? "bg-gray-400 text-white"
                             : "text-gray-300 hover:bg-gray-700 hover:text-white",
                           "px-3 py-2 rounded-md text-sm font-medium"
                         )}
@@ -88,25 +92,28 @@ export default function Navbar() {
             </div>
           </div>
 
-          <Disclosure.Panel className="sm:hidden">
-            <div className="px-2 pt-2 pb-3 space-y-1 bg-black transition-all ease-in-out duration-300">
-              {navigation.map((item) => (
-                <a
-                  key={item.name}
-                  href={item.href}
-                  className={classNames(
-                    item.current
-                      ? "bg-gray-900 text-white"
-                      : "text-gray-300 hover:bg-gray-700 hover:text-white",
-                    "block px-3 py-2 rounded-md text-base font-medium"
-                  )}
-                  aria-current={item.current ? "page" : undefined}
-                >
-                  {item.name}
-                </a>
-              ))}
-            </div>
-          </Disclosure.Panel>
+          <DisclosurePanel className="block md:hidden">
+            {({ close }) => (
+              <div className="px-2 pt-2 pb-3 space-y-1 bg-black transition-all ease-in-out duration-300">
+                {navigation.map((item) => (
+                  <a
+                    onClick={() => close()}
+                    key={item.name}
+                    href={item.href}
+                    className={classNames(
+                      item.current
+                        ? "bg-gray-900 text-white"
+                        : "text-gray-300 hover:bg-gray-700 hover:text-white",
+                      "block px-3 py-2 rounded-md text-base font-medium"
+                    )}
+                    aria-current={item.current ? "page" : undefined}
+                  >
+                    {item.name}
+                  </a>
+                ))}
+              </div>
+            )}
+          </DisclosurePanel>
         </>
       )}
     </Disclosure>
